a193deb84076d5d35e418ac55217e69a50dc93b9
unknown <tjdtn7745@gmail.com>
2022-12-10 23:05:48 +0900

    spring_jpa2_entity_dto_fetch_join

--- spring_boot_jpa_use2_api_develop_optimization/api/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ spring_boot_jpa_use2_api_develop_optimization/api/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -105,5 +105,15 @@ public class OrderRepository {
     public List<Order> findAll() {
         return null;
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                        "select distinct o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d" +
+                                " join fetch o.orderItems oi" +
+                                " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
 
