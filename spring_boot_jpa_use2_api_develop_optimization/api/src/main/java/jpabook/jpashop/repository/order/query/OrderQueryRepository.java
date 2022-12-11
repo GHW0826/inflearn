eb6f991f63b7d956e38c803258c23068714e59da
unknown <tjdtn7745@gmail.com>
2022-12-11 17:25:51 +0900

    spring_data_jpa_domain_check

--- spring_boot_jpa_use2_api_develop_optimization/api/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ spring_boot_jpa_use2_api_develop_optimization/api/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderQueryRepository {
     private final EntityManager em;
+
     /**
      * 컬렉션은 별도로 조회
      * Query: 루트 1번, 컬렉션 N 번
@@ -33,8 +34,8 @@ public class OrderQueryRepository {
         return em.createQuery(
                         "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
                         " from Order o" +
-                                " join o.member m" +
-                                " join o.delivery d", OrderQueryDto.class)
+                        " join o.member m" +
+                        " join o.delivery d", OrderQueryDto.class)
                 .getResultList();
     }
     /**
@@ -44,8 +45,8 @@ public class OrderQueryRepository {
         return em.createQuery(
                         "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                         " from OrderItem oi" +
-                                " join oi.item i" +
-                                " where oi.order.id = : orderId",
+                        " join oi.item i" +
+                        " where oi.order.id = : orderId",
                         OrderItemQueryDto.class)
                 .setParameter("orderId", orderId)
                 .getResultList();
