ee5059ba9afd5363d87bd588858b5a150a392d5d
unknown <tjdtn7745@gmail.com>
2022-12-24 16:12:47 +0900

    querydsl_sort

---
+++ sprin_querydsl/querydsl/src/main/java/study/querydsl/JPAConfig.java
@@ -0,0 +1,24 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+@EnableJpaAuditing
+public class JPAConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
