12865a982ed2f538bcdada73070edbf586d519fd
unknown <tjdtn7745@gmail.com>
2022-12-25 16:34:34 +0900

    spring_db1_db_connection

---
+++ spring_db1/jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbc";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "a4s5d6A4s5!";
+}
