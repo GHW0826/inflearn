bdda044461027b5d7ff8fb875e67c651d7ff46b7
unknown <tjdtn7745@gmail.com>
2023-01-05 21:43:40 +0900

    spring_db1_update_delete

--- spring_db1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ spring_db1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -31,5 +33,38 @@ class MemberRepositoryV0Test {
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    void update() throws SQLException {
+        //save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+    }
+
+    @Test
+    void delete() throws SQLException {
+        //save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
     }
 }
