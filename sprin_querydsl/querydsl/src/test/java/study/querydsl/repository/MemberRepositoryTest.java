5dc3bcca8c8a10263c53d9fe74b5ec372a51a429
unknown <tjdtn7745@gmail.com>
2023-01-16 01:26:54 +0900

    querydsl_QuerydslPredicateExecutor2

--- sprin_querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ sprin_querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -88,11 +88,24 @@ class MemberRepositoryTest {
     
     @Test
     public void querydslPredicateExcutorTest() {
-        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40)
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        Iterable<Member> result = memberRepository.findAll(member.age.between(0, 40)
                         .and(member.username.eq("member1"))
         );
-        for (Member member1 : result) {
-            System.out.println("member1 = " + member1);
+        for (Member member : result) {
+            System.out.println("member1 = " + member);
         }
     }
 }
