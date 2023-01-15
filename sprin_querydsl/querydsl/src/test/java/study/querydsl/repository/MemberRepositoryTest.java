ccffe3d94be63e3dc5158ad74b8669a91562e423
unknown <tjdtn7745@gmail.com>
2023-01-16 01:25:24 +0900

    querydsl_QuerydslPredicateExecutor

--- sprin_querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ sprin_querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -16,6 +17,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -82,5 +84,15 @@ class MemberRepositoryTest {
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
 
+    }
+    
+    @Test
+    public void querydslPredicateExcutorTest() {
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40)
+                        .and(member.username.eq("member1"))
+        );
+        for (Member member1 : result) {
+            System.out.println("member1 = " + member1);
+        }
     }
 }
