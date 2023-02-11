8da1a85e3fe300108de19e96cfbec8a1789eb085
unknown <tjdtn7745@gmail.com>
2023-02-11 16:42:51 +0900

    spring_advanced_@Aspect_proxy_apply

--- spring_advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring_advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -8,6 +8,7 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v4_beanpostprocessor.BeanPostProcessorConfig;
 import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -16,21 +17,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
-// @Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
 // @Import(ConcreteProxyConfig.class)
 // @Import(DynamicProxyBasicConfig.class)
 // @Import(DynamicProxyFilterConfig.class)
 // @Import(ProxyFactoryConfigV1.class)
 // @Import(ProxyFactoryConfigV2.class)
 // @Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+// @Import(AutoProxyConfig.class)
+// @Import(AutoProxyMultiAdvisorConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
-
 	@Bean
 	public LogTrace logTrace() {
 		return new ThreadLocalLogTrace();
