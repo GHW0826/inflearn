8272de217224bc3af1544ff23b8c4aa95ca60250
unknown <tjdtn7745@gmail.com>
2023-02-25 19:22:48 +0900

    spring_security1_RememberMeAuthenticationFilter

--- spring_security1/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ spring_security1/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -80,7 +81,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 .deleteCookies("remember-me");
-
+        
         http.rememberMe()
                 .rememberMeParameter("remember")
                 .tokenValiditySeconds(3600)
