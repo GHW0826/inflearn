3fcdc72e5a2675c3a006c8aea6080e8cf8b4247a
unknown <tjdtn7745@gmail.com>
2023-03-29 23:12:20 +0900

    spring_concurrency_eample_test

--- spring/spring_concurrency/stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ spring/spring_concurrency/stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -38,4 +42,29 @@ class StockServiceTest {
         assertEquals(99, stock.getQuantity());
     }
 
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        // ExecutorService : 비동기로 실행하는 작업을 단순화하여 사용할 수 있도록 도와주는 자바 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 100개의 요청이 끝날때 까지 기다려야해서 사용
+        // 다른 스레드에서 실행중인 작업이 완료될 때까지 대기할수 있도록 도와주는 클래스
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; ++i) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0L, stock.getQuantity());
+    }
 }
