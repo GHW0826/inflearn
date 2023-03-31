2268f25f565ef9c9c7854bb63f6bbbe7b1ef0e20
tjdtn7745@github.com <tjdtn7745@guthub.com>
2023-03-31 14:28:08 +0900

    spring_concurrency_pessimistic_lock

--- spring/spring_concurrency/stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ spring/spring_concurrency/stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -19,7 +19,8 @@ class StockServiceTest {
 
     @Autowired
     private StockService stockService;
-
+    @Autowired
+    private PessimisticLockStockService pessimisticLockStockService;
     @Autowired
     private StockRepository stockRepository;
 
@@ -56,6 +57,32 @@ class StockServiceTest {
             executorService.submit(() -> {
                 try {
                     stockService.decrease(1L, 1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0L, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청_with_Lock() throws InterruptedException {
+        int threadCount = 100;
+        // ExecutorService : 비동기로 실행하는 작업을 단순화하여 사용할 수 있도록 도와주는 자바 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 100개의 요청이 끝날때 까지 기다려야해서 사용
+        // 다른 스레드에서 실행중인 작업이 완료될 때까지 대기할수 있도록 도와주는 클래스
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; ++i) {
+            executorService.submit(() -> {
+                try {
+                    pessimisticLockStockService.decrease(1L, 1L);
                 } finally {
                     countDownLatch.countDown();
                 }
