760bc48210faad4731838b3b0ac4ec8a728dd198
unknown <tjdtn7745@gmail.com>
2023-04-08 23:31:39 +0900

    spring_jpa_basic_field_column_mapping

--- spring/spring_jpa_basic/hello/src/main/java/ex1hello/hello/entity/Member.java
+++ spring/spring_jpa_basic/hello/src/main/java/ex1hello/hello/entity/Member.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter @Setter
@@ -14,6 +14,16 @@ public class Member {
     @Id
     private Long id;
     private String name;
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
+
     //Getter, Setter …
     public Member(Long id, String name) {
         this.id = id;
---
+++ spring/spring_jpa_basic/hello/src/main/java/ex1hello/hello/entity/RoleType.java
@@ -0,0 +1,5 @@
+package ex1hello.hello.entity;
+
+public enum RoleType {
+    USER, ADMIN
+}
