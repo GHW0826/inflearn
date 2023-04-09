48b51af892371f1fed22edbc4e2020949b1a713f
tjdtn7745@github.com <tjdtn7745@guthub.com>
2023-04-09 14:35:42 +0900

    spring_jpa_basic_example

---
+++ spring/spring_jpa_basic/example/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(length = 10)
+    private String name;
+    private String city;
+    private String street;
+    private String zeipcode;
+}
