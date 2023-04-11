76cffcaf42cdcded72c875d249cdbe6a637d9c60
unknown <tjdtn7745@gmail.com>
2023-04-11 23:27:10 +0900

    spring_security1_project_init

--- spring/spring_security1/basicsecurity/src/main/java/io/security/basicsecurity/CsrfConfig.java
+++ spring/spring_security1/basicsecurity/src/main/java/io/security/basicsecurity/CsrfConfig.java
@@ -1,6 +1,10 @@
 package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.ConsensusBased;
+import org.springframework.security.access.vote.UnanimousBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +16,11 @@ public class CsrfConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        AccessDecisionManager y;
+        AffirmativeBased q;
+        ConsensusBased r;
+        UnanimousBased w;
+
         http.authorizeHttpRequests()
                 .anyRequest().authenticated();
         http.formLogin();
