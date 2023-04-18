29830e52895823abe923eab141ce15a490f12ce2
tjdtn7745@github.com <tjdtn7745@guthub.com>
2023-04-18 19:05:17 +0900

    spring_security1_customauthenticationfailurehandler

--- spring/spring_security1/core-spring-security/src/main/java/security/corespringsecurity/security/config/SecurityConfig.java
+++ spring/spring_security1/core-spring-security/src/main/java/security/corespringsecurity/security/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/users", ,"user/login/**", "/login").permitAll()
+                .antMatchers("/", "/users", "user/login/**", "/login").permitAll()
                 .antMatchers("/mypage").hasRole("USER")
                 .antMatchers("/message").hasRole("MANAGER")
                 .antMatchers("/config").hasRole("ADMIN")
