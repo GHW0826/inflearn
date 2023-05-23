853a9922bdab237358887767c1efb24bbe7b2f69
tjdtn7745@github.com <tjdtn7745@guthub.com>
2023-05-23 19:14:21 +0900

    spring_core1_bean_life_cycle_InitializingBean_DisposableBean

--- spring/spring_core_principle/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ spring/spring_core_principle/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -14,11 +14,25 @@ public class BeanLifeCycleTest {
         ac.close(); //스프링 컨테이너를 종료, ConfigurableApplicationContext 필요
     }
 
+    @Test
+    public void lifeCycleInterfaceTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClientInterface client = ac.getBean(NetworkClientInterface.class);
+        ac.close(); //스프링 컨테이너를 종료, ConfigurableApplicationContext 필요
+    }
+
     @Configuration
     static class LifeCycleConfig {
         @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+
+        @Bean
+        public NetworkClientInterface networkClientInterface() {
+            NetworkClientInterface networkClient = new NetworkClientInterface();
             networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
         }
