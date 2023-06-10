ece10832eabdf9c8cb33083b1b54097e7004bc8d
unknown <tjdtn7745@gmail.com>
2023-06-10 23:19:10 +0900

    spring_oauth2_securitybuild_securityconfigurer

--- spring/spring_security2_oauth/src/src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ spring/spring_security2_oauth/src/src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -2,11 +2,15 @@ package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 @SpringBootApplication
 public class SpringSecurityOauth2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringSecurityOauth2Application.class, args);
     }
 
