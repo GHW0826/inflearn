0bdf712fbdde0753ceefa12ddecec11823b360ae
tjdtn7745@github.com <tjdtn7745@guthub.com>
2023-06-14 19:52:13 +0900

    spring_security2_authentication_entry_point

--- spring/spring_security2_oauth/src/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ spring/spring_security2_oauth/src/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -3,7 +3,14 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 // 자동 설정 주석
 @EnableWebSecurity
@@ -12,8 +19,15 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin();
+        // http.formLogin();
+        // http.httpBasic();
         // http.apply(new CustomSecurityConfigurer().setFlag(true));
+        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                System.out.println("custom entryPoint");
+            }
+        });
         return http.build();
     }
     @Bean
