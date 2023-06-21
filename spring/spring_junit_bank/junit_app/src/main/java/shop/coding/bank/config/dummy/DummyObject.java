64b8b7819ff028d6655951cf96fd5a6a8aa2e9e7
unknown <tjdtn7745@gmail.com>
2023-06-21 23:48:49 +0900

    spring_junit_bank_deposit_service_test

--- spring/spring_junit_bank/junit_app/src/main/java/shop/coding/bank/config/dummy/DummyObject.java
+++ spring/spring_junit_bank/junit_app/src/main/java/shop/coding/bank/config/dummy/DummyObject.java
@@ -2,6 +2,8 @@ package shop.coding.bank.config.dummy;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import shop.coding.bank.domain.account.Account;
+import shop.coding.bank.domain.transaction.Transaction;
+import shop.coding.bank.domain.transaction.TransactionEnum;
 import shop.coding.bank.domain.user.User;
 import shop.coding.bank.domain.user.UserEnum;
 
@@ -9,7 +11,27 @@ import java.time.LocalDateTime;
 
 public class DummyObject {
 
-    protected User newUser(String username, String fullname) {
+    // 계좌 1111L 1000원
+    // 입금 트랜잭션 -> 계좌 1100원 변경 -> 입금 트랜잭션 히스토리가 생성되어야 함.
+    protected static Transaction newMockDepositTransaction(Long id, Account account) {
+        account.deposit(100L);
+        Transaction transaction = Transaction.builder()
+                .id(id)
+                .withdrawAccount(null)
+                .depositAccount(account)
+                .withdrawAccountBalance(null)
+                .depositAccountBalance(account.getBalance())
+                .amount(100L)
+                .gubun(TransactionEnum.DEPOSIT)
+                .sender("ATM")
+                .receiver(account.getNumber() + "")
+                .tel("01088887777")
+                .createAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        return transaction;
+    }
+    protected static User newUser(String username, String fullname) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encPassword = passwordEncoder.encode("1234");
         return User.builder()
@@ -21,7 +43,7 @@ public class DummyObject {
                 .build();
     }
 
-    protected User newMockUser(Long id, String username, String fullname) {
+    protected static User newMockUser(Long id, String username, String fullname) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encPassword = passwordEncoder.encode("1234");
         return User.builder()
@@ -36,7 +58,7 @@ public class DummyObject {
                 .build();
     }
 
-    protected Account newAccount(Long number, User user) {
+    protected static Account newAccount(Long number, User user) {
         return Account.builder()
                 .number(number)
                 .password(1234L)
@@ -45,7 +67,7 @@ public class DummyObject {
                 .build();
     }
 
-    protected Account newMockAccount(Long id, Long number, Long balance, User user) {
+    protected static Account newMockAccount(Long id, Long number, Long balance, User user) {
         return Account.builder()
                 .id(id)
                 .number(number)
@@ -56,4 +78,6 @@ public class DummyObject {
                 .updatedAt(LocalDateTime.now())
                 .build();
     }
+
+
 }
--- spring/spring_junit_bank/junit_app/src/test/java/shop/coding/bank/service/AccountServiceTest.java
+++ spring/spring_junit_bank/junit_app/src/test/java/shop/coding/bank/service/AccountServiceTest.java
@@ -10,11 +10,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import shop.coding.bank.config.dummy.DummyObject;
 import shop.coding.bank.domain.account.Account;
 import shop.coding.bank.domain.account.AccountRepository;
+import shop.coding.bank.domain.transaction.Transaction;
+import shop.coding.bank.domain.transaction.TransactionRepository;
 import shop.coding.bank.domain.user.User;
 import shop.coding.bank.domain.user.UserRepository;
 import shop.coding.bank.dto.account.AccountReqDto;
+import shop.coding.bank.dto.account.AccountReqDto.AccountDepositReqDto;
 import shop.coding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import shop.coding.bank.dto.account.AccountRespDto;
+import shop.coding.bank.dto.account.AccountRespDto.AccountDepositRespDto;
 import shop.coding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
 import shop.coding.bank.handler.ex.CustomApiException;
 
@@ -37,8 +41,44 @@ class AccountServiceTest extends DummyObject {
     @Mock
     private AccountRepository accountRepository;
 
+    @Mock
+    private TransactionRepository transactionRepository;
+
     @Spy // 진짜 객체를 InjectMocks에 주입한다.
     private ObjectMapper om;
+
+    // Account -> balance 변경됐는지
+    // Transaction -> balance 잘 기록했는지
+    @Test
+    public void 계좌입금_test() throws Exception {
+        // given
+        AccountDepositReqDto accountDepositReqDto = new AccountDepositReqDto();
+        accountDepositReqDto.setNumber(1111L);
+        accountDepositReqDto.setAmount(100L);
+        accountDepositReqDto.setGubun("DEPOSIT");
+        accountDepositReqDto.setTel("01088887777");
+
+        // stub 1 (첫 2줄은 실행될때 when절은 바로 실행 안됨.) when 절은 실제 서비스가 실행되야 실행됨
+        User ssar = newMockUser(1L, "ssar", "ssar");
+        Account ssarAccount1 = newMockAccount(1L, 1111L, 1000L, ssar); // 실행됨- 계좌 1000원 ssarAccount1 -> 1000
+        when(accountRepository.findByNumber(any())).thenReturn(Optional.of(ssarAccount1));
+
+        // stub 2 (첫 1줄은 실행될때 when절은 바로 실행 안됨.) when 절은 실제 서비스가 실행되야 실행됨
+        // stub이 진생될 때마다 연관된 객체는 새로 만들어야 굿
+        Account ssarAccount2 = newMockAccount(1L, 1111L, 1000L, ssar); // 실행됨- 계좌 1000원 ssarAccount1 -> 1000
+        Transaction transaction = newMockDepositTransaction(1L, ssarAccount2); //실행됨 - 1100원 ssarAccount1 -> 1100 (ssarAccount1시)
+        when(transactionRepository.save(transaction)).thenReturn(transaction);
+
+        // when (when 절 다 실행) mock 객체는 when절마다 생성하지 않으면 싱크 오류가 날 수 있음
+        AccountDepositRespDto accountDepositRespDto = accountService.계좌입금(accountDepositReqDto);
+        System.out.println("트랜잭션 입금계좌 잔액 = " + accountDepositRespDto.getTransaction().getDepositAccountBalance());
+        System.out.println("계좌 잔액 = " + ssarAccount1.getBalance());
+
+        // then
+        assertThat(ssarAccount1.getBalance()).isEqualTo(1100L);
+        assertThat(ssarAccount2.getBalance()).isEqualTo(1100L);
+        assertThat(accountDepositRespDto.getTransaction().getDepositAccountBalance()).isEqualTo(1100L);
+    }
 
     @Test
     public void 계좌삭제_test() throws Exception {
@@ -57,7 +97,6 @@ class AccountServiceTest extends DummyObject {
         assertThrows(CustomApiException.class, ()-> accountService.계좌삭제(number, userId));
 
         // then
-
 
         /*
         try {
