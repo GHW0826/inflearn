ba092dab557e72f6d1e914f1d1581100f7ce6fa3
unknown <tjdtn7745@gmail.com>
2023-06-22 23:25:56 +0900

    spring_junit_bank_service_test_concept

--- spring/spring_junit_bank/junit_app/src/test/java/shop/coding/bank/service/AccountServiceTest.java
+++ spring/spring_junit_bank/junit_app/src/test/java/shop/coding/bank/service/AccountServiceTest.java
@@ -47,6 +47,69 @@ class AccountServiceTest extends DummyObject {
     @Spy // 진짜 객체를 InjectMocks에 주입한다.
     private ObjectMapper om;
 
+    @Test
+    public void 계좌입금_test2() throws Exception {
+        // dto와 특정 값이 잘맞는지만 보면 된다.
+        // dto가 잘 만들어졌는지는 controller에서도 볼 수 있다.
+        // 구지 service테스트에서 해야 할까?
+
+        // TODO
+        // 계좌 출급 테스트
+        // 계좌 이체 테스트
+        // 계좌목록보기 테스트
+        // 계좌 상세보기
+
+        // given
+        AccountDepositReqDto accountDepositReqDto = new AccountDepositReqDto();
+        accountDepositReqDto.setNumber(1111L);
+        accountDepositReqDto.setAmount(100L);
+        accountDepositReqDto.setGubun("DEPOSIT");
+        accountDepositReqDto.setTel("01088887777");
+
+        User ssar1 = newMockUser(1L, "ssar", "ssar");
+        Account ssarAccount1 = newMockAccount(1L, 1111L, 1000L, ssar1); // 실행됨- 계좌 1000원 ssarAccount1 -> 1000
+        when(accountRepository.findByNumber(any())).thenReturn(Optional.of(ssarAccount1));
+
+        User ssar2 = newMockUser(1L, "ssar", "ssar");
+        Account ssarAccount2 = newMockAccount(1L, 1111L, 1000L, ssar2); // 실행됨- 계좌 1000원 ssarAccount1 -> 1000
+        Transaction transaction = newMockDepositTransaction(1L, ssarAccount2); //실행됨 - 1100원 ssarAccount1 -> 1100 (ssarAccount1시)
+        when(transactionRepository.save(any())).thenReturn(transaction);
+
+        // when
+        AccountDepositRespDto accountDepositRespDto = accountService.계좌입금(accountDepositReqDto);
+        String responseBody = om.writeValueAsString(accountDepositRespDto);
+        System.out.println("responseBody = " + responseBody);
+
+        // then
+        assertThat(ssarAccount1.getBalance()).isEqualTo(1100L);
+    }
+
+    // 서비스 테스트. 기술적인 테크닉을 보여주기 위해서 함.
+    // 진짜 서비스를 하고 싶으면, 내가 지금 무엇을 테스트해야할지 명확히 구분해야함(책임 분리)
+    // DTO를 만드는 책임 -> 서비스에 있지만, (서비스에서 DTO검증 안하고 - controller 테스트에서 할수 있음)
+    // DB관련된 것도 -> 서비스 책임이 아니라 안봐도됨 사실.
+    // DB 관련된 것을 조회했을 때, 그 값을 통해서 어떤 비즈니스 로직이 흘러가는 것이 있으면
+    // -> stub으로 정의해서 테스트 해보면 된다.
+
+    // DB 스텁, DB 스텁(가짜로 DB만들어서 deposit 검증... 0원 검증 .. 등)
+    @Test
+    public void 계좌입금_test3() throws Exception {
+
+        // given
+        Account account = newMockAccount(1L, 1111L, 1000L, null);
+        Long amount = 100L;
+
+        // when
+        if (amount <= 0L) {
+            throw new CustomApiException("0원 이하의 금액을 입금할 수 없습니다.");
+        }
+        account.deposit(100L);
+
+        // then
+        assertThat(account.getBalance()).isEqualTo(1100L);
+    }
+
+
     // Account -> balance 변경됐는지
     // Transaction -> balance 잘 기록했는지
     @Test
