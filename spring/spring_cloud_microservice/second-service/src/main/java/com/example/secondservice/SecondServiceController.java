b457c29a1d6edd2c9f1f152aba15297b924241ad
unknown <tjdtn7745@gmail.com>
2023-08-06 21:51:24 +0900

    spring_cloud_microservice_gateway_filter

--- spring/spring_cloud_microservice/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ spring/spring_cloud_microservice/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,14 +1,21 @@
 package com.example.secondservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second Service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        System.out.println(header);
+        return "Hello World in second Service";
     }
 }
