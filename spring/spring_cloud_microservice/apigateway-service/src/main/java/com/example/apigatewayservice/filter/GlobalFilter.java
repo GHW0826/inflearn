589b587a53974f7c5e372711f13e7db375cabf26
unknown <tjdtn7745@gmail.com>
2023-08-12 19:33:45 +0900

    spring_cloud_microservice_cloud_gateway

---
+++ spring/spring_cloud_microservice/apigateway-service/src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
@@ -0,0 +1,49 @@
+package com.example.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // Custom Pre Filter
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            System.out.println("Global Filter baseMessage: " + config.getBaseMessage());
+
+            if (config.isPreLogger()) {
+                System.out.println("Global Filter Start: request id -> " + request.getId());
+            }
+
+            // Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    System.out.println("Global Filter End: response code -> " + response.getStatusCode());
+                }
+            }));
+        };
+    }
+}
--- spring/spring_cloud_microservice/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ spring/spring_cloud_microservice/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,27 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First Service.";
@@ -19,5 +31,12 @@ public class FirstServiceController {
     public String message(@RequestHeader("first-request") String header) {
         System.out.println(header);
         return "Hello World in First Service";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        System.out.println("Server port=" + request.getServerPort());
+        return String.format("Hi, there, This is a message from First Service on Port %s",
+                env.getProperty("local.server.port"));
     }
 }
