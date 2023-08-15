80de576fcd7ff76fe2c5169846ff89d4db788cec
unknown <tjdtn7745@gmail.com>
2023-08-15 16:50:58 +0900

    spring_cloud_microservice_user-service1

---
+++ spring/spring_cloud_microservice/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.example.userservice.controller;
+
+
+import com.example.userservice.dto.ResponseUser;
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.service.UserServiceImpl;
+import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private final UserService userService;
+
+    private final Environment env;
+
+    private final Greeting greeting;
+
+    @Autowired
+    public UserController(UserService userService, Environment env, Greeting greeting) {
+        this.userService = userService;
+        this.env = env;
+        this.greeting = greeting;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        // env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<?> createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+}
